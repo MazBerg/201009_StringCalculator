5db3f417cd4558db631785ab37d7dc39afd4647d
MazBerg <maz@mazberg.com>
2020-10-10 20:07:17 +0200

    Separate Number components

--- index.js
+++ index.js
@@ -13,35 +13,89 @@ solveButton.addEventListener("click", function() {
     const equationCharsOnly = equationCharsAll.filter( (char) => {
         return (char !== " ")
     })
+
+    // Creates Array of Operators
     const equationOperator = equationCharsOnly.filter( (char) => {
         return isNaN(char)
     })
-    const newEquationString = equationCharsOnly.join("")
-    const equationComponents = newEquationString.split(equationOperator)
-    const compA = parseInt(equationComponents[0])
-    const compB = parseInt(equationComponents[1])
+    console.log(equationOperator)
+
+    let newEquationString = equationCharsOnly.join("")
+    //console.log(newEquationString)
+    let finalEqComp = []
+    
+    // 12+30*21+4-15 -> 631 // my test formula
+
+    equationComponents = newEquationString.split(equationOperator[0])
+    console.log("EqC:", equationComponents)
+    console.log(isNaN(equationComponents[1]))
+
+    if (equationOperator.length > 1) {
+        for(let i = 0; i < equationComponents.length; i++){
+            if(isNaN(equationComponents[i])){
+                const temp = equationComponents[i]
+                const tempArr = temp.split("")
+                const tempOp = tempArr.filter((char) => {
+                    return isNaN(char)
+                })
+                console.log("temp:", temp)
+                console.log("tempArr:", tempArr)
+                console.log("tempOp:", tempOp)
+                const tempComp = temp.split(tempOp)
+                console.log("tempComp:", tempComp)
+                tempComp.forEach((comp) => {
+                    finalEqComp.push(comp)
+                })
+            } else {
+                finalEqComp.push(equationComponents[i])
+            }
+        }
+    } else {
+        equationComponents.forEach((comp) => {
+            finalEqComp.push(comp)
+        })
+    }
+
+    console.log("FC: ", finalEqComp)
     
 
-    switch(equationOperator[0]) {
-        case "+":
-            solution = compA + compB
-            break;
-        case "-":
-            solution = compA - compB
-            break;
-        case "*":
-            solution = compA * compB
-            break;
-        case "/" :
-            solution = compA / compB
-            break;
-        default:
-            solution = "Operator was not recognized"
-    }
+    // for(let i = 0; i < equationOperator.length; i++){
+    //     equationComponents = newEquationString.split(equationOperator[i])
+    //     console.log("EqC:", equationComponents)
+    //     finalEqComp.push(equationComponents.shift())
+    // }
+
+    // console.log("FC: ", finalEqComp)
+    
+
+    solution = eval(newEquationString)
+    
+
+    // const equationComponents = newEquationString.split(equationOperator)
+    // const compA = parseInt(equationComponents[0])
+    // const compB = parseInt(equationComponents[1])
+    
+
+    // switch(equationOperator[0]) {
+    //     case "+":
+    //         solution = compA + compB
+    //         break;
+    //     case "-":
+    //         solution = compA - compB
+    //         break;
+    //     case "*":
+    //         solution = compA * compB
+    //         break;
+    //     case "/" :
+    //         solution = compA / compB
+    //         break;
+    //     default:
+    //         solution = "Operator was not recognized"
+    // }
 
     const divElement = document.createElement("div")
     divElement.classList.add("equation-component")
-    divElement.innerText = `${compA} ${equationOperator[0]} ${compB} = ${solution}`
+    divElement.innerText = `${solution}`
 
     document.getElementById("solution-display").appendChild(divElement)
     
