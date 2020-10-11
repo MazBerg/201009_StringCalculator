ca1d36cb03080b6e28ae7a4054f67a0e70f0c1e4
MazBerg <maz@mazberg.com>
2020-10-11 19:49:11 +0200

    Minor Cleanup

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ solveButton.addEventListener("click", function() {
         }
     }
 
-    console.log("EqChars: ", equationCharsOnly)     // <--------------
-    console.log("EqOp: ", equationOperator)         // <--------------
-    console.log("OpPos: ", operatorPos)             // <--------------
+    // console.log("EqChars: ", equationCharsOnly)     // <--------------
+    // console.log("EqOp: ", equationOperator)         // <--------------
+    // console.log("OpPos: ", operatorPos)             // <--------------
     
     let newEquationString = equationCharsOnly.join("")
     
@@ -50,10 +50,12 @@ solveButton.addEventListener("click", function() {
             equationCharsOnly[i]=" "
         }
     }
-    console.log("EqChars 2: ", equationCharsOnly)   // <--------------
+    // console.log("EqChars 2: ", equationCharsOnly)   // <--------------
+
     const tempString = equationCharsOnly.join("")
     const finalEqComp = tempString.split(" ")
-    console.log("FinEqComp", finalEqComp)           // <--------------
+    // console.log("FinEqComp", finalEqComp)           // <--------------
+
     while(finalEqComp.indexOf("") !== -1){
         for(let i = 0; i < finalEqComp.length; i++){
             if(finalEqComp[i] === ""){
@@ -62,13 +64,13 @@ solveButton.addEventListener("click", function() {
         }
     }
     
-    console.log("FinEqComp 2", finalEqComp)         // <--------------
+    // console.log("FinEqComp 2", finalEqComp)         // <--------------
 
     for(let i = 0; i < equationOperator.length; i++){
         finalEqComp.splice(operatorPos[i], 0, equationOperator[i])
     }
 
-    console.log("FinEqComp 3", finalEqComp)         // <--------------
+    // console.log("FinEqComp 3", finalEqComp)         // <--------------
 
     for(let i = 0; i < finalEqComp.length; i++){
         const divElement = document.createElement("div")
@@ -111,47 +113,3 @@ solveButton.addEventListener("click", function() {
     document.getElementById("solution-display").appendChild(divSolElement)
     
 })
-
-/*
-Part 1 (Calculation): 
-    +Your first goal is to solve a simple text-based
-        math problem entered in the input field
-    +The problem can be add/sub/multiply/divide
-    +Here are few examples: 
-        "3 + 3" -> 6
-        "10 - 3" -> 7
-        "44 / 2" -> 22
-        "2 * 8" -> 16 
-    +When the 'Solve' button is clicked
-        -Create a new div with the
-            class 'equation-component'
-            its text value should be the solution
-            to the input equation
-        -This element should be added as a child of 
-            the `solutionDisplay` div
-
-    Note: You can assume there will always only be 2 values, 
-        both whole integers, and always a space between each 
-        integer and the operator as in the above examples
-
-
-Part 2 (Flex Display): 
-    Then, you'll Flex your Flexbox skills!
-    + Vertically stack the contents of the mainContainer
-    + Center the content horizontally
-    + Display all components of the equation 
-        in the solutionDisplay using a horizontal Flexbox
-        with `space around` each component
-    
-Skills: 
-    Event Listeners, String Manipulation, Array Manipulation, 
-Arithmetic, DOM Manipulation, Flexbox
-
-
-
-STRETCH GOALS:
-    +Accept and solve more complex problems with more than 2 inputs
-    +Signal the different types of components (operator/value/solution) with different colors
-    +Accept strings without spaces
-    +Can you improve the overall design?
-*/
